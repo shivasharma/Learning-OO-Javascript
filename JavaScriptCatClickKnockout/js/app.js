de3ce45e563ef54fb09b5e57b745da651e6fefbc
CIPHER\s.sharma <s.sharma@cipher-sys.com>
2018-04-29 23:25:01 -0400

    reformat code

--- JavaScriptCatClickKnockout/js/app.js
+++ JavaScriptCatClickKnockout/js/app.js
@@ -1,49 +1,49 @@
 
-var Cat=function(data){
+var Cat = function (data) {
 
-    this.clickCount=ko.observable(data.clickCount)
-    this.name=ko.observable(data.name);
-    this.imgSrc=ko.observable(data.imgSrc);
-    this.imgAttribution=ko.observable(data.imgAttribution);
-    this.nickNames=ko.observableArray(data.nickNames);
+    this.clickCount = ko.observable(data.clickCount)
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nickNames = ko.observableArray(data.nickNames);
 
-    this.title=ko.computed(function(){
+    this.title = ko.computed(function () {
         var title;
-        var clicks=this.clickCount();
-        if(clicks<10){
-            title='NewBorn';
-        }else if(click<50){
-            title='Infant';
-        }else if(click<100){
-            title='Child';
-        }else if(click<200){
-            title='Teen';
-        }else if(click<500){
-            title='Adult';
-        }else{
-            title='Ninja';
+        var clicks = this.clickCount();
+        if (clicks < 10) {
+            title = 'NewBorn';
+        } else if (click < 50) {
+            title = 'Infant';
+        } else if (click < 100) {
+            title = 'Child';
+        } else if (click < 200) {
+            title = 'Teen';
+        } else if (click < 500) {
+            title = 'Adult';
+        } else {
+            title = 'Ninja';
         }
-        return title;       
-    },this)
+        return title;
+    }, this)
 
 }
 
-var ViewModel=function(){
- 
-    var self=this;
-    self.catList=ko.observableArray([]);
-   //var loadCats=new initialiCats();
-   initialiCats.forEach(function(catItem){
+var ViewModel = function () {
+
+    var self = this;
+    self.catList = ko.observableArray([]);
+    //var loadCats=new initialiCats();
+    initialiCats.forEach(function (catItem) {
         self.catList.push(new Cat(catItem));
     });
 
-    self.currentCat=ko.observable(self.catList()[0]);
+    self.currentCat = ko.observable(self.catList()[0]);
 
-    self.incrementCounter=function(){
-    self.currentCat().clickCount(self.currentCat().clickCount()+1);
+    self.incrementCounter = function () {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 
-    self.setCat=function(clickedCat){
+    self.setCat = function (clickedCat) {
         self.currentCat(clickedCat);
     }
 }
--- JavaScriptCatClickKnockout/js/data.js
+++ JavaScriptCatClickKnockout/js/data.js
@@ -1,25 +1,25 @@
-var initialiCats=(function(){
-return [{
-    clickCount:0,
-    name:'Tabby',
-    imgSrc:'img/a.jpg',
-    imgAttribution:'www.google.com',
-    nickNames:['Tiger']
-},{
-    clickCount:0,
-    name:'Tummy',
-    imgSrc:'img/b.jpg',
-    imgAttribution:'www.google.com',
-    nickNames:['Shobby']
+var initialiCats = (function () {
+    return [{
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/a.jpg',
+        imgAttribution: 'www.google.com',
+        nickNames: ['Tiger']
+    }, {
+        clickCount: 0,
+        name: 'Tummy',
+        imgSrc: 'img/b.jpg',
+        imgAttribution: 'www.google.com',
+        nickNames: ['Shobby']
 
-},{
-    clickCount:0,
-    name:'Yummy',
-    imgSrc:'img/c.jpg',
-    imgAttribution:'www.google.com',
-    nickNames:['zzzz']
+    }, {
+        clickCount: 0,
+        name: 'Yummy',
+        imgSrc: 'img/c.jpg',
+        imgAttribution: 'www.google.com',
+        nickNames: ['zzzz']
 
-}]
+    }]
 })();
 
 
